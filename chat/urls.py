9a3a9800fc978f042bc7cf93ba0aa8ce01b2f5a9
md shariful islam <sharifulcsehstu@gmail.com>
2019-10-22 00:40:10 +0600

    chat application progressing

--- chat/urls.py
+++ chat/urls.py
@@ -2,5 +2,6 @@ from django.urls import path
 from chat import views
 
 urlpatterns = [
-    path('<room_name>/', views.Chat.as_view(), name='chat'),
+    #path('<room_name>/', views.ChatView.as_view(), name='chat'),
+    path('<room_name>/', views.room, name='chat'),
 ]
--- chat/views.py
+++ chat/views.py
@@ -1,5 +1,6 @@
 import json
 
+from django.contrib.auth.decorators import login_required
 from django.shortcuts import render
 from django.utils.safestring import mark_safe
 from rest_framework import status
@@ -8,7 +9,7 @@ from rest_framework.response import Response
 from rest_framework.views import APIView
 
 
-class Chat(APIView):
+class ChatView(APIView):
     renderer_classes = [TemplateHTMLRenderer]
     template_name = 'chat.html'
     def get(self,request,room_name):
@@ -16,3 +17,13 @@ class Chat(APIView):
 
     def post(self,request):
         return Response({'success_code':"success"},status = status.HTTP_200_OK)
+
+@login_required
+def room(request,room_name):
+    return render(
+        request,'chat.html',
+    {
+        'room_name_json':mark_safe(json.dumps(room_name)),
+        'username': mark_safe(json.dumps(request.user.username)),
+    }
+    )
